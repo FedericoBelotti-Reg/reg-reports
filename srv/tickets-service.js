4a324771d9867348c5778a8e4cd181b40461deda
Federico Belotti <federico.belotti@regestaitalia.it>
2023-12-04 14:23:58 +0000

    Improved TicketsService handlers - Added guard functions to handlers to avoid useless logic execution.

--- srv/tickets-service.js
+++ srv/tickets-service.js
@@ -11,39 +11,36 @@ class TicketsService extends cds.ApplicationService {
 
         // /Tickets?$expand=reports.
         this.on("READ", Tickets, async (req, next) => {
-            const select = req.query.SELECT;
-
             // If no column has been requested, serve data through the next handler.
-            if (!select.columns) {
+            if (!req.query.SELECT.columns) {
                 return next();
             }
 
             // Finding expand column for Reports entity.
             const reportsExpand = req.query.SELECT.columns.find(({ expand, ref }) => expand && ref.find(r => r === "reports"));
 
-            // Removing eventual expand columns.
+            // If no expand for Reports entity has been found, serve data through the next handler.
+            if (!reportsExpand) {
+                return next()
+            }
+
+            // Removing expand columns.
             req.query.SELECT.columns = req.query.SELECT.columns.filter(({ expand }) => !expand);
 
             // Retrieving requested entity data.
             const tickets = await api.run(req.query);
             const ticketIds = Array.isArray(tickets) ? tickets.map(ticket => ticket.ID) : [tickets.ID];
 
-            // If the expand for Reports has been found.
-            if (reportsExpand) {
-                // Adding ID field for expand entity if not already requested.
-                if (reportsExpand.expand.indexOf("*") == -1 && !reportsExpand.expand.find(column => column.ref && column.ref.find(ref => ref === "ticket_ID"))) {
-                    reportsExpand.expand.push({ ref: ["ticket_ID"] });
-                }
-
-                // Get reports for the given ticket_IDs.
-                const reports = await cds.run(SELECT.from(Reports).columns(reportsExpand.expand).where({ ticket_ID: { in: ticketIds } }));
-
-                // If any report has been found, push the expanded data to the main entity result set.
-                return tickets.map((ticket) => ({ ...ticket, reports: reports.filter(report => report.ticket_ID == ticket.ID) }))
+            // Adding ID field for expand entity if not already requested.
+            if (reportsExpand.expand.indexOf("*") == -1 && !reportsExpand.expand.find(column => column.ref && column.ref.find(ref => ref === "ticket_ID"))) {
+                reportsExpand.expand.push({ ref: ["ticket_ID"] });
             }
 
-            // If no expand has been requested, serve data through the next handler.
-            return next();
+            // Get reports for the given ticket_IDs.
+            const reports = await cds.run(SELECT.from(Reports).columns(reportsExpand.expand).where({ ticket_ID: { in: ticketIds } }));
+
+            // If any report has been found, push the expanded data to the main entity result set.
+            return tickets.map((ticket) => ({ ...ticket, reports: reports.filter(report => report.ticket_ID == ticket.ID) }))
         });
 
         // /Tickets
